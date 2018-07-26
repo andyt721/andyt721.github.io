fb116ceb2d62c4582dc4757ebe478b5614e8be2e
Andy Terpstra <andy.terpstra@hotmail.ca>
2018-07-26 12:50:31 -0400

    update 1 2018.07.26

--- js/main.js
+++ js/main.js
@@ -3,34 +3,77 @@ $(document).ready(function(){
   $('.product-bar a').hover(function () {
     $(this).siblings().toggleClass('show-image');
   });
+
+  //Loading large-header
+  $('#large-header').load('../large-header.html', function() {
+    alert('Load was performed');
+  });
+
   //Setting up the billboard transitions
-  $('#billboard-background').addClass('billboard-default');
+  let currentBillboard = 1;
+  $('#billboard-background').addClass('billboard-one');
   $('#billboard-one').addClass('current-billboard-button');
-  $('#billboard-one').click(function () {
-    $('#billboard-two').removeClass('current-billboard-button');
-    $('#billboard-three').removeClass('current-billboard-button');
-    $('#billboard-one').addClass('current-billboard-button');
-    $('#billboard-background').removeClass('billboard-default');
-    $('#billboard-background').removeClass('billboard-two');
-    $('#billboard-background').removeClass('billboard-three');
-    $('#billboard-background').addClass('billboard-one');
+  $('#billboard-one').click(function() {
+    switch (currentBillboard) {
+      case 1:
+        break;
+      case 2:
+        $('#billboard-two').removeClass('current-billboard-button');
+        $('#billboard-one').addClass('current-billboard-button');
+        $('#billboard-background').removeClass('billboard-two');
+        $('#billboard-background').addClass('billboard-one');
+        break;
+      case 3:
+        $('#billboard-three').removeClass('current-billboard-button');
+        $('#billboard-one').addClass('current-billboard-button');
+        $('#billboard-background').removeClass('billboard-three');
+        $('#billboard-background').addClass('billboard-one');
+        break;
+      default:
+        break;
+    }
+    currentBillboard = 1;
   });
-  $('#billboard-two').click(function () {
-    $('#billboard-two').addClass('current-billboard-button');
-    $('#billboard-three').removeClass('current-billboard-button');
-    $('#billboard-one').removeClass('current-billboard-button');
-    $('#billboard-background').removeClass('billboard-default');
-    $('#billboard-background').removeClass('billboard-three');
-    $('#billboard-background').removeClass('billboard-one');
-    $('#billboard-background').addClass('billboard-two');
+  $('#billboard-two').click(function() {
+    switch (currentBillboard) {
+      case 1:
+        $('#billboard-one').removeClass('current-billboard-button');
+        $('#billboard-two').addClass('current-billboard-button');
+        $('#billboard-background').removeClass('billboard-one');
+        $('#billboard-background').addClass('billboard-two');
+        break;
+      case 2:
+        break;
+      case 3:
+        $('#billboard-three').removeClass('current-billboard-button');
+        $('#billboard-two').addClass('current-billboard-button');
+        $('#billboard-background').removeClass('billboard-three');
+        $('#billboard-background').addClass('billboard-two');
+        break;
+      default:
+        break;
+    }
+    currentBillboard = 2;
   });
-  $('#billboard-three').click(function () {
-    $('#billboard-two').removeClass('current-billboard-button');
-    $('#billboard-three').addClass('current-billboard-button');
-    $('#billboard-one').removeClass('current-billboard-button');
-    $('#billboard-background').removeClass('billboard-default');
-    $('#billboard-background').removeClass('billboard-one');
-    $('#billboard-background').removeClass('billboard-two');
-    $('#billboard-background').addClass('billboard-three');
+  $('#billboard-three').click(function() {
+    switch (currentBillboard) {
+      case 1:
+        $('#billboard-one').removeClass('current-billboard-button');
+        $('#billboard-three').addClass('current-billboard-button');
+        $('#billboard-background').removeClass('billboard-one');
+        $('#billboard-background').addClass('billboard-three');
+        break;
+      case 2:
+        $('#billboard-two').removeClass('current-billboard-button');
+        $('#billboard-three').addClass('current-billboard-button');
+        $('#billboard-background').removeClass('billboard-two');
+        $('#billboard-background').addClass('billboard-three');
+        break;
+      case 3:
+        break;
+      default:
+        break;
+    }
+    currentBillboard = 3;
   });
 });
